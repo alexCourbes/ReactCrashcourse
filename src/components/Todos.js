827f05c18dddce7877b13cff8f6d8e41177eadc8
Alex <courbesalex@gmail.com>
2020-06-11 15:25:36 +0100

    Todo component

---
+++ src/components/Todos.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+class Todos extends Component{
+    render() {
+        return (
+            <div className="App">
+                <h1>TodoApp</h1>
+            </div>
+
+        )
+    }
+
+}
+export default Todos;
